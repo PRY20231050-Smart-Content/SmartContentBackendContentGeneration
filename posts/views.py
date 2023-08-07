9b53b8d3f96dc5e0304a3cc070c9b6aaeb302655
BrigitteMendez1302 <U201914955@upc.edu.pe>
2023-08-07 01:08:31 -0500

    add: post

--- posts/views.py
+++ posts/views.py
@@ -8,55 +8,55 @@ from rest_framework.decorators import api_view
 
 
 class PostsView(APIView):
-    def post(self, request):
-        try:
-            first_name = request.data.get('first_name')
-            last_name = request.data.get('last_name')
-            address_id = request.data.get('address_id')
-            email = request.data.get('email')
-            phone = request.data.get('phone')
-            profile_picture = request.data.get('profile_picture')
-            user_id = request.data.get('user_id')
+    # def post(self, request):
+    #     try:
+    #         first_name = request.data.get('first_name')
+    #         last_name = request.data.get('last_name')
+    #         address_id = request.data.get('address_id')
+    #         email = request.data.get('email')
+    #         phone = request.data.get('phone')
+    #         profile_picture = request.data.get('profile_picture')
+    #         user_id = request.data.get('user_id')
 
-            with connection.cursor() as cursor:
-                 cursor.execute("CALL insert_client(%s, %s, %s, %s, %s, %s, %s)", [first_name, last_name, address_id, email, phone, profile_picture, user_id])
+    #         with connection.cursor() as cursor:
+    #              cursor.execute("CALL insert_client(%s, %s, %s, %s, %s, %s, %s)", [first_name, last_name, address_id, email, phone, profile_picture, user_id])
 
-            return Response({'message': 'Client created.'}, status=status.HTTP_201_CREATED)
+    #         return Response({'message': 'Client created.'}, status=status.HTTP_201_CREATED)
 
-        except Exception as e:
-            # Puedes registrar la excepción aquí para depurarla posteriormente
-            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    #     except Exception as e:
+    #         # Puedes registrar la excepción aquí para depurarla posteriormente
+    #         return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-    def put(self, request):
-        try:
-            client_id = request.data.get('client_id')
-            first_name = request.data.get('first_name')
-            last_name = request.data.get('last_name')
-            address_id = request.data.get('address_id')
-            email = request.data.get('email')
-            phone = request.data.get('phone')
-            profile_picture = request.data.get('profile_picture')
-            user_id = request.data.get('user_id')
+    # def put(self, request):
+    #     try:
+    #         client_id = request.data.get('client_id')
+    #         first_name = request.data.get('first_name')
+    #         last_name = request.data.get('last_name')
+    #         address_id = request.data.get('address_id')
+    #         email = request.data.get('email')
+    #         phone = request.data.get('phone')
+    #         profile_picture = request.data.get('profile_picture')
+    #         user_id = request.data.get('user_id')
 
-            with connection.cursor() as cursor:
-                 cursor.execute("CALL update_client(%s, %s, %s, %s, %s, %s, %s, %s)", [client_id, first_name, last_name, address_id, email, phone, profile_picture, user_id])
+    #         with connection.cursor() as cursor:
+    #              cursor.execute("CALL update_client(%s, %s, %s, %s, %s, %s, %s, %s)", [client_id, first_name, last_name, address_id, email, phone, profile_picture, user_id])
             
-            return Response({'message': 'Client updated.'}, status=status.HTTP_200_OK)
+    #         return Response({'message': 'Client updated.'}, status=status.HTTP_200_OK)
 
-        except Exception as e:
-            # Puedes registrar la excepción aquí para depurarla posteriormente
-            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    #     except Exception as e:
+    #         # Puedes registrar la excepción aquí para depurarla posteriormente
+    #         return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
-    def get(self, request):
+    def post(self, request):
         page = request.data.get('npage', 1)
         perpage = request.data.get('perPage', 10)
         sortby = request.data.get('sortBy', 'created_at')
         sortOrder = request.data.get('sortOrder', 'ASC')
         filterDateFrom = request.data.get('date_from')
         filterDateTo = request.data.get('date_to')
-        text = request.data.get('text', '')
-        businessId = request.data.get('businessId', '')
+        text = request.data.get('text', None)
+        businessId = request.data.get('businessId', None)
 
         params = [
             
@@ -72,6 +72,7 @@ class PostsView(APIView):
         ]
       
         try:
+            pass
             with connection.cursor() as cursor:
                 cursor.callproc('sp_get_posts', params)
                 data = cursor.fetchall()
@@ -116,28 +117,28 @@ class PostsView(APIView):
         except Exception as e:
             return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-    def getClientById(self, request, client_id):
-        try:
-            with connection.cursor() as cursor:
-                cursor.execute("SELECT * FROM clients WHERE id = %s", [client_id])
-                data = cursor.fetchone()
+    # def getClientById(self, request, client_id):
+    #     try:
+    #         with connection.cursor() as cursor:
+    #             cursor.execute("SELECT * FROM clients WHERE id = %s", [client_id])
+    #             data = cursor.fetchone()
 
-            if data:
-                client_details = {
-                    'id': data[0],
-                    'first_name': data[1],
-                    'last_name': data[2],
-                    'address_id': data[3],
-                    'email': data[4],
-                    'phone': data[5],
-                    'profile_picture': data[6],
-                    'user_id': data[7],
-                    # Include other fields as needed
-                }
+    #         if data:
+    #             client_details = {
+    #                 'id': data[0],
+    #                 'first_name': data[1],
+    #                 'last_name': data[2],
+    #                 'address_id': data[3],
+    #                 'email': data[4],
+    #                 'phone': data[5],
+    #                 'profile_picture': data[6],
+    #                 'user_id': data[7],
+    #                 # Include other fields as needed
+    #             }
 
-                return Response(client_details, status=status.HTTP_200_OK)
+    #             return Response(client_details, status=status.HTTP_200_OK)
 
-            return Response({'message': 'Client not found.'}, status=status.HTTP_404_NOT_FOUND)
+    #         return Response({'message': 'Client not found.'}, status=status.HTTP_404_NOT_FOUND)
 
-        except Exception as e:
-            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    #     except Exception as e:
+    #         return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
