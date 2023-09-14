404751aeddc59fd4f4cd1cb5828ffb40387b490d
Carolina <carolina.villegas@alarcongroup.us>
2023-09-13 22:32:42 -0500

    Ad

--- SmartContentBackendContentGeneration/settings.py
+++ SmartContentBackendContentGeneration/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'SmartContentBackendContentGeneration.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'tesis2',
+        'NAME': 'tesis',
         'USER': 'root',
         'PASSWORD': 'root',
         'HOST': 'localhost',
